f48e8e694db44c7a7064dc3849e13f31b140fabc
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-21 20:51:10 +0530

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,20 @@
+# this is the simple Programme
+from Module1 import *
+
+a = 10
+b = 5
+
+c = add(a, b)
+print("Add:-", c)
+
+d = sub(a, b)
+print("sub:-", d)
+
+e = mul(a, b)
+print("Mul:-", e)
+
+f = div(a, b)
+print("Div:-", f)
+
+g = mod(a, b)
+print("Mod:-", g)
